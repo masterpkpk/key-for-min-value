aa348014db8c06d3c73e1a6c56a796727c004a3f
william <masterpkpk@gmail.com>
2020-11-13 14:12:59 -0500

    commit tree 142b21cea7fca37363cb78d2ebb33fb5e188eb2c

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,24 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    num = 10000
+    names = []
+    if name_hash == {}
+        return nil
+    elsif name_hash.each do |name, value|
+        
+        if num > value 
+            num = value
+            names = name
+        
+        end
+    end
+    end
+    names
 end
+
+
+
+# 1. iterate through hash 
+# 2. determine if value is smaller than the last value
+# 3. return samllest value
